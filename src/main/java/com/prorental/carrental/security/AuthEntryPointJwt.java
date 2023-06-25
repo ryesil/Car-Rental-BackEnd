183bf7ccf0307744e9c01f471a8624a99526a011
ryesil <ramazanysl@gmail.com>
2023-06-25 12:40:51 -0500

    webSecurityConfig added

---
+++ src/main/java/com/prorental/carrental/security/AuthEntryPointJwt.java
@@ -0,0 +1,25 @@
+package com.prorental.carrental.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.slf4j.Logger;
+import java.io.IOException;
+
+
+
+//This is for the user who is not entitled to go to a page.
+//Simply we error out. Or we can direct them to the login page
+public class AuthEntryPointJwt  implements AuthenticationEntryPoint {
+
+    private static final Logger Logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    Logger.error("Unauthorized error ${}", authException.getMessage());
+    response.sendError(HttpServletResponse.SC_BAD_GATEWAY, "Error:Unauthorized");
+    }
+}
